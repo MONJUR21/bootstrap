3c44b8990e48f7bb7699aba3957d896394b2b990
MONJUR21 <monjur.csecu@gmail.com>
2024-03-01 02:17:45 +0600

    edited

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-button1=document.getElementById("button1");
-form1=document.getElementById("form1");
-pTag=document.getElementById("p-tag");
+// button1=document.getElementById("button1");
+// form1=document.getElementById("form1");
+// pTag=document.getElementById("p-tag");
 
-button1.addEventListener("click",()=>{
-    text=form1.value;
-    pTag.innerText=text;
-})
+// button1.addEventListener("click",()=>{
+//     text=form1.value;
+//     pTag.innerText=text;
+// })
